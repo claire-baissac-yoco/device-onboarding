5cb01e1c073585c5984fc0a03e469b237debf759
Claire Baissac <claire.baissac@yoco.com>
2022-02-28 14:23:34 +0200

    Set state of device during onboarding

--- device.py
+++ device.py
@@ -1,10 +1,20 @@
+from warehouse import Warehouse
+from const import status_keys
+from sim_card import SimCard
+
 class Device:
-    def __init__(self, serial_number, imei, box_number, crate_number, is_damaged):
+    def __init__(self, serial_number: str):
         self._serial_number = serial_number
+        self._state = status_keys['serial_number_recorded']
+
+    def set_imei(self, imei: int) -> None:
         self._imei = imei
+        self._state = status_keys['imei_recorded']
+
+    def set_box_and_crate(self, box_number: int, crate_number: int) -> None:
         self._box_number = box_number
         self._crate_number = crate_number
-        self._is_damaged = is_damaged
+        self._state = status_keys['package_info_recorded']
 
     def get_serial_number(self):
         return self._serial_number
@@ -21,6 +31,26 @@ class Device:
     def get_is_damaged(self):
         return self._is_damaged
 
-    def assign_sim_card(self, snn, imsi):
-        self._snn = snn
-        self._imsi = imsi
+    def assign_sim_card(self, simcard: SimCard):
+        self._sim_card = simcard
+        self._state = status_keys['sim_card_assigned']
+
+    def get_sim_card(self):
+        return self._sim_card
+
+    def flash(self):
+        self._status = status_keys['device_flashed']
+
+    def get_status(self):
+        return self._status
+
+    def inject_keys(self, keys: list):
+        self._keys = keys
+        self._status = status_keys['keys_injected']
+
+    def send_for_repacking(self):
+        self._status = status_keys['sent_for_repacking']
+
+    def set_warehouse(self, warehouse: Warehouse):
+        self._warehouse = warehouse
+        self._status = status_keys['stored_in_warehouse']
--- test_device.py
+++ test_device.py
@@ -1,5 +1,11 @@
+import pytest
 from device import Device
 from mock_database import mockDatabase
+from const import status_keys
+from custom_error import FlashFailureException, InjectionFailureException
+
+def gen_keys():
+    return [0], [0], [0]
 
 def get_dummy_device_info():
     serial_number = 1
@@ -9,9 +15,20 @@ def get_dummy_device_info():
     is_damaged = False
     return serial_number, imei, box_number, crate_number, is_damaged
 
-def test_can_create_device():
+def create_dummy_device() -> Device:
     serial_number, imei, box_number, crate_number, is_damaged = get_dummy_device_info()
     device = Device(serial_number, imei, box_number, crate_number, is_damaged)
+    return device
+
+def create_database_with_dummy_device() -> mockDatabase:
+    serial_number, imei, box_number, crate_number, is_damaged = get_dummy_device_info()
+    database = mockDatabase()
+    database.create_device(serial_number, imei, box_number, crate_number, is_damaged)
+    return database
+
+def test_can_create_device():
+    serial_number, imei, box_number, crate_number, is_damaged = get_dummy_device_info()
+    device = create_dummy_device()
     assert device.get_serial_number() == serial_number
     assert device.get_imei() == imei
     assert device.get_box_number() == box_number
@@ -19,19 +36,53 @@ def test_can_create_device():
     assert device.get_is_damaged() == is_damaged
 
 def test_can_add_device_to_device_list():
-    serial_number, imei, box_number, crate_number, is_damaged = get_dummy_device_info()
-    database = mockDatabase()
-    database.create_device(serial_number, imei, box_number, crate_number, is_damaged)
+    database = create_database_with_dummy_device()
     assert database.get_number_of_devices() == 1
 
 def test_can_get_device_from_device_list_by_serial_number():
-    serial_number, imei, box_number, crate_number, is_damaged = get_dummy_device_info()
-    database = mockDatabase()
-    database.create_device(serial_number, imei, box_number, crate_number, is_damaged)
+    serial_number  = get_dummy_device_info()[0]
+    database = create_database_with_dummy_device()
     assert database.get_device_by_serial_number(serial_number).get_serial_number() == serial_number
 
 def test_can_get_device_from_device_list_by_imei():
-    serial_number, imei, box_number, crate_number, is_damaged = get_dummy_device_info()
-    database = mockDatabase()
-    database.create_device(serial_number, imei, box_number, crate_number, is_damaged)
+    imei = get_dummy_device_info()[1]
+    database = create_database_with_dummy_device()
     assert database.get_device_by_imei(imei).get_imei() == imei
+
+def test_return_none_when_get_device_not_exist():
+    database = create_database_with_dummy_device()
+    assert database.get_device_by_imei(0) == None
+
+def test_can_update_device_sim_card():
+    serial_number  = get_dummy_device_info()[0]
+    database = create_database_with_dummy_device()
+    database.update_device_sim_card(serial_number, 1, 2)
+    sim_card = database.get_device_by_serial_number(serial_number).get_sim_card()
+    assert sim_card.get_snn() == 1
+    assert sim_card.get_imsi() == 2
+
+def test_can_flash_device():
+    serial_number  = get_dummy_device_info()[0]
+    database = create_database_with_dummy_device()
+    database.flash_device(serial_number, 1)
+    assert database.get_device_by_serial_number(serial_number).get_status() == status_keys['device_flashed']
+
+def test_flash_device_raises_exception():
+    with pytest.raises(FlashFailureException):
+        serial_number  = get_dummy_device_info()[0]
+        database = create_database_with_dummy_device()
+        database.flash_device(serial_number, 0)
+
+def test_can_inject_keys():
+    serial_number  = get_dummy_device_info()[0]
+    database = create_database_with_dummy_device()
+    keys = gen_keys()
+    database.inject_keys(serial_number, keys, 1)
+    assert database.get_device_by_serial_number(serial_number).get_status() == status_keys['keys_injected']
+
+def test_inject_keys_raises_exception():
+    with pytest.raises(InjectionFailureException):
+        serial_number  = get_dummy_device_info()[0]
+        database = create_database_with_dummy_device()
+        keys = gen_keys()
+        database.inject_keys(serial_number, keys, 0)
