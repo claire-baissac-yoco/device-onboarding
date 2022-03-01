a0ba3630db35d9995c805a3c98ba256ef3fbd5c9
Claire Baissac <claire.baissac@yoco.com>
2022-03-01 09:32:58 +0200

    Getters for sim card

--- sim_card.py
+++ sim_card.py
@@ -2,3 +2,9 @@ class SimCard:
     def __init__(self, snn, imsi):
         self._snn = snn
         self._imsi = imsi
+
+    def get_snn(self):
+        return self._snn
+
+    def get_imsi(self):
+        return self._imsi
