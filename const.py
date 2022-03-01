ba6ea9c7c9d64d7aea70dd0e05faf88cefcc1b79
Claire Baissac <claire.baissac@yoco.com>
2022-03-01 09:34:12 +0200

    Updated device states

--- const.py
+++ const.py
@@ -1,14 +1,31 @@
-status_keys = {
-    'serial_number_recorded': 1,
-    'imei_recorded': 2,
-    # 'box_number_recorded': 3,
-    'package_info_recorded': 4,
-    'damage_recorded': 5,
-    'sim_card_assigned': 6,
-    'snn_recorded': 7,
-    'imsi_recorded': 8,
-    'device_flashed': 9,
-    'keys_injected': 10,
-    'sent_for_repacking': 11,
-    'stored_in_warehouse': 12
-}
+from enum import Enum
+
+class DeviceState(Enum):
+    device_received = 1,
+    serial_number_recorded = 2,
+    imei_recorded = 3,
+    package_info_recorded = 4,
+    damage_recorded = 5,
+    sim_card_assigned = 6,
+    # snn_recorded = 7,
+    # imsi_recorded = 8,
+    device_flashed = 7,
+    keys_injected = 8,
+    sent_for_repacking = 9,
+    stored_in_warehouse = 10
+
+# status_keys = {
+#     'device_received': 1,
+#     'serial_number_recorded': 2,
+#     'imei_recorded': 3,
+#     # 'box_number_recorded': 3,
+#     'package_info_recorded': 4,
+#     'damage_recorded': 5,
+#     'sim_card_assigned': 5,
+#     'snn_recorded': 7,
+#     'imsi_recorded': 8,
+#     'device_flashed': 9,
+#     'keys_injected': 10,
+#     'sent_for_repacking': 11,
+#     'stored_in_warehouse': 12
+# }
