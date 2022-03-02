fdc5148d9597f5e2af77c81ca7bd74ee1f804588
Claire Baissac <claire.baissac@yoco.com>
2022-03-02 15:30:24 +0200

    added default error messages to custom exceptions

--- custom_error.py
+++ custom_error.py
@@ -1,11 +1,15 @@
 class InjectionFailureException(Exception):
-    pass
+    def __init__(self, msg='Failed to key inject device', *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
 
 class FlashFailureException(Exception):
-    pass
+    def __init__(self, msg='Failed to flash device', *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
 
 class InvalidOperationException(Exception):
-    pass
+    def __init__(self, msg='This operation is prohibited for the given device', *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
 
 class NoSuchDeviceException(Exception):
-    pass
+    def __init__(self, msg='No such device exists', *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
