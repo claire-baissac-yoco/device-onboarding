e8c234090bba3e8fb0da198d45e784857ef91bb9
Claire Baissac <claire.baissac@yoco.com>
2022-03-02 15:31:05 +0200

    Display exceptions to user

--- app.py
+++ app.py
@@ -14,48 +14,67 @@ def main(ctx):
 @click.option('--serial_number', prompt='Enter the serial number of the device')
 @click.option('--imei', prompt='Enter the imei number of the device')
 def create_device(serial_number, imei):
-    database.create_device(serial_number, imei)
-    print(database.get_number_of_devices())
+    try:
+        database.create_device(serial_number, imei)
+        click.echo('Device successfully registered')
+    except Exception as err:
+        click.echo(f'ERROR: {err}')
 
 @main.command('record-package')
 @click.option('--serial_number', prompt='Enter the serial number of the device')
 @click.option('--box_number', prompt='Enter the box number of the device')
 @click.option('--crate_number', prompt='Enter the crate number of the device')
 def set_device_package_info(serial_number, box_number, crate_number):
-    database.set_device_package_info(serial_number, box_number, crate_number)
+    try:
+        database.set_device_package_info(serial_number, box_number, crate_number)
+    except Exception as err:
+        click.echo(f'ERROR: {err}')
 
 @main.command('record-damage')
 @click.option('--serial_number', prompt='Enter the serial number of the device')
 @click.option('--damage_level', type=click.Choice(['none', 'slight', 'severe']),  prompt='Enter the degree of damage to the device')
 def record_damage(serial_number, damage_level):
-    database.set_device_damage_rating(serial_number, damage_level)
+    try:
+        database.set_device_damage_rating(serial_number, damage_level)
+    except Exception as err:
+        click.echo(f'ERROR: {err}')
 
 @main.command('assign-sim')
 @click.option('--snn', prompt='Enter the SNN for the SIM card')
 @click.option('--imsi', prompt='Enter the IMSI for the SIM card')
 @click.option('--serial_number', prompt='Enter the serial number of the device')
 def assign_sim(snn, imsi, serial_number):
-    print(snn, imsi, serial_number)
-    database.update_device_sim_card(serial_number, snn, imsi)
+    try:
+        database.update_device_sim_card(serial_number, snn, imsi)
+    except Exception as err:
+        click.echo(f'ERROR: {err}')
 
 @main.command('flash')
 @click.option('--serial_number', prompt='Enter the serial number of the device')
 def flash_device(serial_number):
     print(serial_number)
-    database.flash_device(serial_number, 0)
+    try:
+        database.flash_device(serial_number, 1)
+    except Exception as err:
+        click.echo(f'ERROR: {err}')
 
 @main.command('inject-key')
 @click.option('--keys', prompt='Enter key to inject into the device')
 @click.option('--serial_number', prompt='Enter the serial number of the device')
 def inject_key(keys, serial_number):
-    print(keys, serial_number)
-    database.inject_keys(serial_number, keys, 0)
+    try:
+        database.inject_keys(serial_number, keys, 1)
+    except Exception as err:
+        click.echo(f'ERROR: {err}')
 
 @main.command('info')
 @click.option('--serial_number', prompt='Enter the serial number of the device')
 def get_device_info(serial_number):
-    device = database.get_device_by_serial_number(serial_number)
-    click.echo(device.get_state())
+    try:
+        device = database.get_device_by_serial_number(serial_number)
+        click.echo(f'Current state of device: {device.get_state()}')
+    except Exception as err:
+        click.echo(f'ERROR: {err}')
 
 if __name__ == '__main__':
     main(obj = {})
