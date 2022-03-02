d49fc1c15f1889edeafe8e4f9daf3c5a0d4549be
Claire Baissac <claire.baissac@yoco.com>
2022-03-02 13:55:52 +0200

    Refactor unit tests

--- test_device_onboarding.py
+++ test_device_onboarding.py
@@ -12,8 +12,8 @@ def get_dummy_device_info():
     imei = 1
     box_number = 1
     crate_number = 1
-    is_damaged = False
-    return serial_number, imei, box_number, crate_number, is_damaged
+    damage_rating = 0
+    return serial_number, imei, box_number, crate_number, damage_rating
 
 def create_dummy_device() -> Device:
     serial_number, imei = get_dummy_device_info()[0:2]
@@ -28,11 +28,11 @@ def create_database_with_dummy_device() -> mockDatabase:
 
 def update_device_to_given_state(database: mockDatabase, serial_number, state) -> mockDatabase:
     func_names = ['', '', '', 'set_device_package_info', 'set_device_damage_rating', 'update_device_sim_card', 'flash_device', 'inject_keys', 'send_device_for_repacking', 'set_device_warehouse']
-    box_number, crate_number, is_damaged = get_dummy_device_info()[2:]
+    box_number, crate_number, damage_rating = get_dummy_device_info()[2:]
     snn, imsi = 1, 2
     mode = 1
     keys = gen_keys()
-    func_params = ['', '', '', f'{box_number}, {crate_number}', f'{is_damaged}', f'{snn}, {imsi}', f'{mode}', 'keys, mode', '', '[1, 2, 3, 4, 5, 6]']
+    func_params = ['', '', '', f'{box_number}, {crate_number}', f'{damage_rating}', f'{snn}, {imsi}', f'{mode}', 'keys, mode', '', '[1, 2, 3, 4, 5, 6]']
     last = False
     for st, name, params in zip(DeviceState, func_names, func_params):
         print(st, name, params)
@@ -40,7 +40,7 @@ def update_device_to_given_state(database: mockDatabase, serial_number, state) -
             last = True
         if not name == '':
             params = f', {params}' if params else ''
-            to_eval = f'database.{name}(serial_number{params})'
+            to_eval = f'database.{name}({serial_number}{params})'
             print(to_eval)
             eval(to_eval)
 
@@ -77,6 +77,23 @@ def test_can_record_package_info():
     database = create_database_with_dummy_device()
     database.set_device_package_info(serial_number, box_number, crate_number)
     assert database.get_device_by_serial_number(serial_number).get_state() == DeviceState.package_info_recorded
+
+def test_can_record_device_damage():
+    serial_number  = get_dummy_device_info()[0]
+    database = create_database_with_dummy_device()
+    database = update_device_to_given_state(database, serial_number, DeviceState.package_info_recorded)
+    database.set_device_damage_rating(serial_number, 0)
+    assert database.get_device_by_serial_number(serial_number).get_damage_rating() == 0
+    assert database.get_device_by_serial_number(serial_number).get_state() == DeviceState.damage_recorded
+
+def test_cannot_proceed_if_device_damaged():
+    with pytest.raises(InvalidOperationException):
+        serial_number  = get_dummy_device_info()[0]
+        database = create_database_with_dummy_device()
+        database = update_device_to_given_state(database, serial_number, DeviceState.package_info_recorded)
+        database.set_device_damage_rating(serial_number, 1)
+        database.update_device_sim_card(serial_number, 1, 1)
+    assert database.get_device_by_serial_number(serial_number).get_state() == DeviceState.damage_recorded
 
 def test_can_update_device_sim_card():
     serial_number  = get_dummy_device_info()[0]
